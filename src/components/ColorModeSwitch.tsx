bfe99109efe449e375279f71601d490ace1ec6ff
Devin Anugrah <devinanugrahp27@gmail.com>
2024-08-31 21:48:13 +0700

    created theme swtich

---
+++ src/components/ColorModeSwitch.tsx
@@ -0,0 +1,19 @@
+import { HStack, Switch, Text, useColorMode } from "@chakra-ui/react";
+
+const ColorModeSwitch = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+  const isDarkMode = colorMode === "dark";
+
+  return (
+    <HStack>
+      <Switch
+        isChecked={isDarkMode}
+        onChange={toggleColorMode}
+        colorScheme="green"
+      />
+      <Text>{isDarkMode ? "Dark" : "Light"} Mode</Text>
+    </HStack>
+  );
+};
+
+export default ColorModeSwitch;
