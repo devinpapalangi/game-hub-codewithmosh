2314e62f7d8115f3447027f728537b88073d5769
Devin Anugrah <devinanugrahp27@gmail.com>
2024-08-31 23:23:25 +0700

    added criting score

---
+++ src/components/CriticScore.tsx
@@ -0,0 +1,20 @@
+import { Badge } from "@chakra-ui/react";
+import React from "react";
+
+interface Props {
+  score: number;
+}
+const CriticScore: React.FC<Props> = ({ score }) => {
+  return (
+    <Badge
+      paddingX={2}
+      fontSize={"14px"}
+      borderRadius={"4px"}
+      colorScheme={score > 75 ? "green" : score > 60 ? "yellow" : "red"}
+    >
+      {score}
+    </Badge>
+  );
+};
+
+export default CriticScore;
