7e21cd251a7f8a43298480ba65c422f0b7f158ab
Devin Anugrah <devinanugrahp27@gmail.com>
2024-10-19 17:46:31 +0700

    done

--- src/layouts/layout.tsx
+++ src/layouts/layout.tsx
@@ -1,12 +1,11 @@
 import { Outlet } from "react-router-dom";
-import { Box, Text } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
 import Navbar from "../components/Navbar";
 
 const Layout = () => {
   return (
     <>
       <Navbar />
-      <Text>Test CI CD</Text>
       <Box padding={5}>
         <Outlet />
       </Box>
