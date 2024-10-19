66512344cdfc1f51c7b5f0b556b28dd7d3a9e78c
Devin Anugrah <devinanugrahp27@gmail.com>
2024-10-19 16:26:53 +0700

    test ci cd is changing content

--- src/layouts/layout.tsx
+++ src/layouts/layout.tsx
@@ -1,11 +1,12 @@
 import { Outlet } from "react-router-dom";
-import { Box } from "@chakra-ui/react";
+import { Box, Text } from "@chakra-ui/react";
 import Navbar from "../components/Navbar";
 
 const Layout = () => {
   return (
     <>
       <Navbar />
+      <Text>Test CI CD</Text>
       <Box padding={5}>
         <Outlet />
       </Box>
